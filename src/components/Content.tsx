9c626feb8f2ecfa73176f7243634cee54f71a547
Robson Quedevez <robson.quedevez@hotmail.com>
2022-01-30 19:04:56 -0300

    Challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,42 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+
+import '../styles/content.scss';
+
+interface GenreResponseProps {
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface IContentProps {
+  selectedGenre: GenreResponseProps,
+  movies: MovieProps[]
+}
+
+
+export function Content({ selectedGenre, movies }: IContentProps) {
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
